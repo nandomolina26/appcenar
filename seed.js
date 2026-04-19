6977c114cd6fe1b447f9be0e72ff32a5571dac4f
nandomolina26 <multinando26@gmail.com>
2026-04-19 18:33:40 -0400

    remove exposed credentials from seed.js

--- seed.js
+++ seed.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+require('dotenv').config({ path: '.env.development' });
 
-mongoose.connect('mongodb://mongo:eWjNfkpVQcJVkNZWQTlbYldMSGaXZMYG@roundhouse.proxy.rlwy.net:46832').then(async () => {
+mongoose.connect(process.env.MONGO_URI).then(async () => {
   const User = require('./models/User');
 
   const existe = await User.findOne({ usuario: 'admin' });
@@ -23,8 +24,6 @@ mongoose.connect('mongodb://mongo:eWjNfkpVQcJVkNZWQTlbYldMSGaXZMYG@roundhouse.pr
   });
 
   console.log('Administrador creado exitosamente');
-  console.log('Usuario: admin');
-  console.log('Password: admin123');
   process.exit();
 
 }).catch(err => {
